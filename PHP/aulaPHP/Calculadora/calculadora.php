e86c5ee977adb07ed86755e4d845381616b5791e
MaryBr <marina_neri@hotmail.com>
2018-09-24 10:35:55 -0300

    Calculadora
    Uma simples calculadora utilizando os conceitos de hml, css e php.

---
+++ PHP/aulaPHP/Calculadora/calculadora.php
@@ -0,0 +1,9 @@
+<?php 
+
+	$num1 = isset($_GET["numero"])?$_GET["numero"]: "[erro]";	
+	$num2 = isset($_GET["numero"])?$_GET["numero"]: "[erro]";
+	$op   = isset ($_GET["operador"])?$_GET["operador"]: "[erro]";
+	
+	switch($)
+
+?>
