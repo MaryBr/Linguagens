6b9fa27f9c9506376999a80a66f8aa7eb9027563
MaryBr <marina_neri@hotmail.com>
2018-10-01 14:11:01 -0300

    Add files via upload
    Simples calculadora em php usando conceito de classe e o comando switch

--- PHP/aulaPHP/Calculadora/calculadora.php
+++ PHP/aulaPHP/Calculadora/calculadora.php
@@ -1,9 +1,52 @@
-<?php 
+<?php
 
-	$num1 = isset($_GET["numero"])?$_GET["numero"]: "[erro]";	
-	$num2 = isset($_GET["numero"])?$_GET["numero"]: "[erro]";
-	$op   = isset ($_GET["operador"])?$_GET["operador"]: "[erro]";
+class Calculadora
+{
 	
-	switch($)
+	function calculo()
+	{
+		if(isset($_POST["resultado"]))
+		{
+				$num1 = $_POST["num1"];
+				$num2 = $_POST["num2"];
+				$op = $_POST["operador"];
+				
+				$result=0;
+		
+				switch ($op) {
+					case '+':
+						$result=$num1 + $num2;
+						return $result;
+						
+						
+					case '-':
+						$result=$num1 - $num2;
+						return $result;
+					
+						break;
+					case '*':
+						$result=$num1 * $num2;
+						return $result;
+				
+						break;
+					case '/':
+						if ($num2==0) {
+							/* "Não existe divisão por zero.";*/
+							return -1;
+						}
+						$result=$num1 /$num2;
+						return $result;
+						break;
+				}
+			}
+	}
+}
+
+	 # Instancia a classe CALCULADORA()
+        $calcular = new Calculadora();
+
+        # Executa a função
+         $var= $calcular->calculo();
+         echo "O resultado da operação é: $var.";
 
 ?>
